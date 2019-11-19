ff42caa376aa2100235ce6bdde8e956a62611bd3
josepanz <josepanza1@gmail.com>
2019-11-19 12:52:33 -0300

    se agrego las referencias de los formularios al menu principal

--- demo_bibliografia/menu.vb
+++ demo_bibliografia/menu.vb
@@ -14,4 +14,27 @@
     Private Sub SalirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SalirToolStripMenuItem.Click
         Me.Close()
     End Sub
+
+    Private Sub TsmLibro_Click(sender As Object, e As EventArgs) Handles tsmLibro.Click
+        Dim form As New libro
+
+        form.Show()
+    End Sub
+
+    Private Sub TsmAutor_Click(sender As Object, e As EventArgs) Handles tsmAutor.Click
+        Dim form As New autor
+
+        form.Show()
+    End Sub
+
+    Private Sub TsmEdicion_Click(sender As Object, e As EventArgs) Handles tsmEdicion.Click
+        Dim form As New edicion
+
+        form.Show()
+    End Sub
+
+    Private Sub TsmEditorial_Click(sender As Object, e As EventArgs) Handles tsmEditorial.Click
+        Dim form As New editorial
+        form.Show()
+    End Sub
 End Class
